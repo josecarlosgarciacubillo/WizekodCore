b1ced295ad7afde8333be330187d438d74148811
José Carlos García Cubillo <carlos.garcia@wizeline.com>
2021-03-06 13:42:12 -0600

    Refactored first Networking API to include dependency injection for easier testing.

--- Sources/WizekodCore/Networking.swift
+++ Sources/WizekodCore/Networking.swift
@@ -7,6 +7,20 @@
 
 import Foundation
 
+protocol NetworkSession {
+  func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void)
+}
+
+extension URLSession: NetworkSession {
+  
+  func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+    let task = dataTask(with: url) { (data, _, error) in
+      completionHandler(data, error)
+    }
+    task.resume()
+  }
+}
+
 extension WizekodCore {
   
   public class Networking {
@@ -17,14 +31,17 @@ extension WizekodCore {
       
       public init() { }
       
-      private let session = URLSession.shared
+      internal var session: NetworkSession = URLSession.shared
       
+      /// Calls to the live Internet to retrieve Data from a specific location
+      /// - Parameters:
+      ///   - url: The location you wish to fetch data from
+      ///   - completionHandler: Returns a result object which signifies the status of the request
       public func loadData(from url: URL, completionHandler: @escaping (NetworkResult<Data>) -> Void) {
-        let task = session.dataTask(with: url) { (data, response, error) in
+        session.get(from: url) { (data, error) in
           let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
           completionHandler(result)
         }
-        task.resume()
       }
     }
     
